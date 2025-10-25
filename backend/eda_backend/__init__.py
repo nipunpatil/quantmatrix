6f9186e0fd7d5fc572d040ee23b22a2e61328c97
nipunpatil <nipunpatil2004@gmail.com>
2025-10-25 16:13:14 +0530

    intial setup

---
+++ backend/eda_backend/__init__.py
@@ -0,0 +1,13 @@
+import os    
+from celery import Celery 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eda_backend.settings')
+
+app = Celery ('eda_backend')
+
+app.config_from_object ('django.conf:settings' , namespace = 'CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
